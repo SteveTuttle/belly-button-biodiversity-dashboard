b14b55ad78e3be8cb56aa16a5a933c5c89b1cd2a
SteveTuttle <127702378+SteveTuttle@users.noreply.github.com>
2023-08-17 18:22:54 -0400

    continue fixing

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -9,7 +9,7 @@ Criteria for the Bonus:
     * You will need to modify the example gauge code to account for values ranging from 0 through 9. []
     * Update the chart whenever a new sample is selected. []
 */
-
+// Build gauge chart (with needle pointer)
 function bonusGauge(wfreq) {
 
 let gaugeData = [
@@ -18,7 +18,7 @@ let gaugeData = [
         value: wfreq,
         title: { text: 'Belly Button Washing Frequency <br> Scrubs per Week' },
         type: 'indicator',
-        mode: 'gauge+number',
+        mode: 'gauge',        
         gauge: {
             axis: { range: [null, 9] },
             steps: [
@@ -34,11 +34,49 @@ let gaugeData = [
               { range: [8, 9], color: "#1b5e20" },
             ],           
         },
+        
     }
 ];
 
+// Calculate coordinates and direction of the needle pointer
+let angle = 180 - (wfreq / 9) * 180;
+let r = 0.4;
+let x_head = r * Math.cos(Math.PI / 180 * angle);
+let y_head = r * Math.sin(Math.PI / 180 * angle);
 
-let gaugeLayout = { width: 600, height: 500, margin: { t: 0, b: 0},  };
+// Proportions of gauge needle radius
+let arrowBaseWidth = 0.08; 
+let arrowPointWidth = 0.02; 
+
+// Arrow head and base points of the gauge needle
+let arrowShape = [
+    // Left base point
+    0.5 - arrowBaseWidth / 2, 0.23,
+    // Right base point
+    0.5 + arrowBaseWidth / 2, 0.23,
+    // Arrow points
+    0.5 + arrowPointWidth / 2 + x_head, 0.23 + y_head,
+    0.5 - arrowPointWidth / 2 + x_head, 0.23 + y_head
+];
+
+let arrowPath = `M ${arrowShape[0]} ${arrowShape[1]} L ${arrowShape[2]} ${arrowShape[3]} L ${arrowShape[4]} ${arrowShape[5]} L ${arrowShape[6]} ${arrowShape[7]} Z`;
+
+
+let gaugeLayout = {
+    xaxis: { range: [0, 1], showgrid: false, zeroline: false, visible: false },
+    yaxis: { range: [0, 1], showgrid: false, zeroline: false, visible: false },
+    showlegend: false,
+    shapes: [
+        {
+            type: 'path',
+            path: arrowPath,
+            fillcolor: 'darkred',
+            line: { color: 'darkred' }
+        }
+    ]
+};
+
+
 
 // build a guage-chart at <div id="gauge"></div> from index.html
 Plotly.newPlot('gauge', gaugeData, gaugeLayout);
